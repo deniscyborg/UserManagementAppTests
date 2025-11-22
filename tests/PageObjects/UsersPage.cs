d248585b1828b8b99810d4b795f4006400d92465
Denis Davydov <larrez@mail.ru>
2025-11-23 01:43:20 +0300

    Fix test

--- tests/PageObjects/UsersPage.cs
+++ tests/PageObjects/UsersPage.cs
@@ -1,23 +1,14 @@
-using NUnit.Framework;
-using Allure.NUnit.Attributes;
-using Allure.Net.Commons;
 using Microsoft.Playwright;
-using PageObjects;
 using System.Threading.Tasks;
 
-namespace Tests
+namespace PageObjects
 {
-    [AllureSuite("UI: Users Page")]
-    public class UsersTests : PageTest      // Или другой base-класс, если используешь PageTest
+    public class UsersPage : BasePage
     {
-        [Test]
-        [AllureTag("ui", "users", "page-object")]
-        [AllureSeverity(SeverityLevel.normal)]
-        public async Task EditUserTest()
-        {
-            var usersPage = new UsersPage(Page); // Page есть только если наследуешь PageTest!
-            await usersPage.FirstEditButton.ClickAsync();
-            // ...добавь шаги редактирования...
-        }
+        public UsersPage(IPage page) : base(page) { }
+
+        // Здесь твои методы для работы со страницей пользователей
+        public async Task ClickFirstEditButtonAsync() =>
+            await Page.ClickAsync("#editBtn1");
     }
 }
